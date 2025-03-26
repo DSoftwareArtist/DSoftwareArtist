cc4b50312256b959ad823480be2fd0a678bd1a05
Reamon Sumapig <29reamonsumapig@gmail.com>
2025-03-26 08:55:00 +0800

    refactor the animate scroll component

--- components/animatedScroll.vue
+++ components/animatedScroll.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex max-w-2xl h-2xl justify-start overflow-auto no-scrollbar group">
-        <div class="flex gap-4 group-hover:[animation-play-state:paused]" :class="direction === 'left' ? 'animate-left-scroll': 'animate-right-scroll'">
+        <div class="flex gap-5 group-hover:[animation-play-state:paused]" :class="direction === 'left' ? 'animate-left-scroll': 'animate-right-scroll'">
             <slot></slot>
         </div>
     </div>
