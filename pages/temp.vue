ef7b221a4fa8f3228ccf429fee83a01068a629de
Reamon Sumapig <29reamonsumapig@gmail.com>
2025-03-26 08:55:29 +0800

    on going landing page improvements

--- pages/temp.vue
+++ pages/temp.vue
@@ -25,20 +25,29 @@ const { data: devops } = await useAsyncData('tools-devops', () => {
     .all()
 })
 
+useAnimatedGrid()
 </script>
 
 <template>
+  <background id="canvas-wrapper" class="transparent block overflow-hidden absolute z-0 left-0 top-0 right-0 bottom-0">
+    <canvas id="canvas-grid-cells" class=" block absolute"></canvas>
+    <canvas id="canvas-grid-lines" class=" block absolute"></canvas>
+  </background>
   <div class="flex w-screen h-screen bg-primary-900">
+    
     <div class="flex flex-col gap-5 justify-center items-start">
       <AnimatedScroll :direction="'left'"> <ToolCards :tools="frontend ?? []"/></AnimatedScroll>
       <AnimatedScroll :direction="'right'"> <ToolCards :tools="backend ?? []"/></AnimatedScroll>
       <AnimatedScroll :direction="'left'"> <ToolCards :tools="devops ?? []"/></AnimatedScroll>
-    </div>
-    <div class="flex justify-center items-center w-full">
-      <div class="bg-secondary-300/8 w-full h-[500px] m-20 border-2 border-primary-400 rounded-2xl p-20 text-white">
-        Soon...
       </div>
-    </div>
+      <div class="flex justify-center items-center w-full">
+        <div class="bg-primary-600 backdrop-blur-2xl w-full h-[610px] mr-20 border-1 border-secondary-200/20 rounded-tr-2xl rounded-br-2xl p-20 text-white">
+          <div class="flex items-center h-full">
+            <div class=" w-3/4"></div>
+            <NuxtImg class="w-60 opacity-60" src="/img/user5.png" />
+          </div>
+        </div>
+      </div>
   </div>
 
 </template>
