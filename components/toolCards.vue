3f1e422e0fb887c53c07a0f96c427c344aa4fe07
Reamon Sumapig <29reamonsumapig@gmail.com>
2025-03-28 08:54:06 +0800

    update the toolCard border

--- components/toolCards.vue
+++ components/toolCards.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="bg-secondary-200/2 hover:bg-secondary-100 border-1 border-secondary-100 backdrop-brightness-90 rounded-xl p-10 cursor-pointer" v-for="tool in props.tools">
+    <div class="bg-secondary-200/2 hover:bg-secondary-100 border-1 border-primary-400 backdrop-brightness-90 rounded-xl p-10 cursor-pointer" v-for="tool in props.tools">
       <div class="flex justify-center">
           <Render class="w-[60px]" :name="tool.svg"/>
         </div>
-        <div class="w-[200px] text-center text-secondary-200 pt-5 text-lg">{{  tool.title }}</div>
+        <div class="w-[200px] text-center text-primary-400 pt-5 text-lg">{{  tool.title }}</div>
     </div>
 </template>
 
